022403062147afdb1c1bf1986aaca1ec0feea93f
Phil Schilter <phil.schilter@gmail.com>
2017-01-02 14:07:55 +0100

    :sparkles: switch from local memory to nedb

--- 03-Express/1-Express/2_demoData/services/orderStore.js
+++ 03-Express/1-Express/2_demoData/services/orderStore.js
@@ -1,34 +1,38 @@
-var orders = [];
+const Datastore = require("nedb");
+const db = new Datastore({ filename: "./data/order.db", autoload: true });
 
 function Order(pizzaName, orderedBy) {
-  this.id = orders.length;
   this.orderedBy = orderedBy;
   this.pizzaName = pizzaName;
   this.orderDate = JSON.stringify(new Date());
   this.state = "OK";
 }
 
-
-function publicAddOrder(pizzaName, orderedBy) {
+function publicAddOrder(pizzaName, orderedBy, callback) {
   var order = new Order(pizzaName, orderedBy);
-  orders.push(order);
-  return order;
+  db.insert(order, function(err, newDoc) {
+    if (callback) {
+      callback(err, newDoc);
+    }
+  });
 }
 
-function publicRemove(id) {
-  var order = publicGet(id);
-  if (order) {
-    order.state = "DELETED";
-  }
-  return order;
+function publicRemove(id, callback) {
+  db.update({ _id: id }, { $set: { "state": "DELETED" } }, {}, function (err, doc) {
+    publicGet(id, callback);
+  });
 }
 
-function publicGet(id) {
-  return orders[id];
+function publicGet(id, callback) {
+  db.findOne({ _id: id }, function(err, doc) {
+    callback(err, doc);
+  });
 }
 
-function publicAll() {
-  return orders;
+function publicAll(callback) {
+  db.find({}, function(err, doc) {
+    callback(err, doc);
+  });
 }
 
 module.exports = {
