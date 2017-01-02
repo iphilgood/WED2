2f4d5aee3fafeb07f2dc7aae3c14202283ab1c34
Phil Schilter <phil.schilter@gmail.com>
2017-01-02 13:39:15 +0100

    add express lecture

---
+++ 03-Express/1-Express/5_demoDataWithLogin/services/userStore.js
@@ -0,0 +1,45 @@
+const crypto = require('crypto');
+
+var Datastore = require('nedb');
+var db = new Datastore({ filename: './data/user.db', autoload: true });
+
+function hashPwd(pwd) {
+    return crypto.createHmac('sha256', "secret!") //more information: https://nodejs.org/api/crypto.html
+        .update(pwd)
+        .digest('hex');
+}
+
+
+function User(email, passwort)
+{
+    this.email = email;
+    this.passwortHash = hashPwd(passwort);
+}
+
+
+function publicRegisterUser(email, passwort, callback)
+{
+    if(!(email && passwort)) {  callback("no user", null); }
+
+    var user = new User(email, passwort);
+    db.insert(user, function(err, newDoc){
+        if(callback){
+            callback(err, newDoc);
+        }
+    });
+}
+
+function publicAuthentication(email, passwort, callback){
+    if(!(email && passwort)) {  callback(false); }
+
+    db.findOne({ email: email }, function (err, doc) {
+        if(doc == null && !err){
+            publicRegisterUser(email, passwort, callback);
+        }
+        else {
+            callback(err, doc && doc.passwortHash == hashPwd(passwort));
+        }
+    });
+}
+
+module.exports = {add : publicRegisterUser, authenticate : publicAuthentication};
---
+++ 03-Express/1-Express/6_demoDataREST/public/js/restClient.js
@@ -0,0 +1,28 @@
+;(function($) {
+
+    ajaxUtil = window.ajax;
+    var token = undefined;
+
+
+    function login(userName, pwd) {
+        return ajaxUtil.ajax("POST", "/login/", { email: userName, pwd: pwd }).done(function (msg) {
+            token = msg;
+        });
+    }
+
+    function logout() {
+        return ajaxUtil.ajax("POST", "/logout/", { token : token } ).done(function (msg) {
+            token = undefined;
+        });
+    }
+
+    function createPizza(pizzeName) {
+        return ajaxUtil.ajax("POST", "/orders/", {name: pizzeName, token: token});
+    }
+   
+    function isLoggedIn() {
+        return !!token;
+    }
+
+    window.restClient = { login: login, logout: logout, createPizza: createPizza, isLogin: isLoggedIn };
+}(jQuery));
